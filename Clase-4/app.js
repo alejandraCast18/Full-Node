e497a25378bdfeeda274adbaca37b2e2a2592463
Alejandra <alejandra@localhost.localdomain>
2024-11-21 19:12:54 -0400

    intensive node

--- Clase-4/app.js
+++ Clase-4/app.js
@@ -1,28 +1,15 @@
-import express, { json } from 'express' // require -> commonJS
-import { moviesRouter } from './routes/movies.js'
-import { corsMiddleware } from './middlewares/cors.js'
+import express, { json } from 'express'; // require -> commonJS
+import { moviesRouter } from './routes/movies.js';
+import { corsMiddleware } from './middlewares/cors.js';
 
-const app = express()
-app.use(json())
-app.use(corsMiddleware())
-app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
+const app = express();
+app.use(json());
+app.disable('x-powered-by');
 
-// métodos normales: GET/HEAD/POST
-// métodos complejos: PUT/PATCH/DELETE
+app.use('/movies', moviesRouter);
 
-app.get('/movies', todo)
-
-app.get('/movies/:id', todo)
-
-app.post('/movies', todo)
-
-app.delete('/movies/:id', todo)
-
-app.patch('/movies/:id', todo)
-
-app.use('/movies', moviesRouter)
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
-  console.log(`server listening on port http://localhost:${PORT}`)
-})
+  console.log(`server listening on port http://localhost:${PORT}`);
+});
